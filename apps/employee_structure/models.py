b0221e359ae38380410f2f89dededeb79e5ab133
Borisoid <boris.lazarev90@mail.ru>
2021-02-15 01:50:27 +0300

    add employee_structure model

--- apps/employee_structure/models.py
+++ apps/employee_structure/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.db import (
+    models,
+)
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=90, null=False, blank=False)
+
+
+class Workgroup(models.Model):
+    name = models.CharField(max_length=90, null=False, blank=False)
+    department = models.ForeignKey(to=Department, on_delete=models.CASCADE)
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=90, null=False, blank=False)
+    ocupation = models.CharField(max_length=90, null=False, blank=False)
+    experience = models.PositiveIntegerField()
+    salary = models.DecimalField(max_digits=8, decimal_places=2)
+    vacation_days = models.PositiveIntegerField()
+
+    workgroup = models.ForeignKey(
+        to=Workgroup, on_delete=models.SET_NULL, null=True
+    )
